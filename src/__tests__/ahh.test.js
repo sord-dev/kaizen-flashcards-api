b37521744538e08228cc3482a01f6ca1a9281af1
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-04-17 10:58:41 +0100

    basic test

---
+++ src/__tests__/ahh.test.js
@@ -0,0 +1,10 @@
+describe("Starter Tests", () => {
+    it('Addition', () => {
+        expect(2 + 2).toEqual(4)
+    });
+
+    it('Equasion', () => {
+        expect(2 == 2).toBe(true)
+    });
+    
+})
