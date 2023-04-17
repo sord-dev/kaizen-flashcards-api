d31e061a5865016c46280e53393cdec2b3bd1ce2
Jack <jackkevinperkins03@gmail.com>
2023-04-17 17:10:14 +0100

    added more routes

--- src/routes/Card_router.js
+++ src/routes/Card_router.js
@@ -1,5 +1,11 @@
 const {Router} = require("express");
-const card_controller = require("")
+const card_controller = require("../Controller/Card_controller")
 const middleWare = require("")
 
 const card_router = Router();
+
+card_router.get("/",card_controller.)
+card_router.get("/:cardid",card_controller)
+card_router.post("/cards",card_controller)
+card_router.put("/:cardid",card_controller)
+card_router.delete("/:cardid",card_controller)
--- src/routes/Deck_router.js
+++ src/routes/Deck_router.js
@@ -1,9 +1,10 @@
 const {Router} = require("express");
 const deck_controller = require("")
+const card_router = require("./Card_router")
 const middleWare = require("")
 
 const deck_router = Router();
-deck_router.use("")
+deck_router.use("/card",card_router)
 
 deck_router.get("/",deck_controller)
 deck_router.get("/:deck_id",deck_controller)
