beb2aa133b19b0feb45ac5fa4dc45c758f36d40d
farhan3311 <farhansidat3311@gmail.com>
2023-04-17 16:35:53 +0100

    response type checked

--- src/__tests__/endpoints.test.js
+++ src/__tests__/endpoints.test.js
@@ -75,11 +75,13 @@ describe("Deck Endpoint Tests", () => {
         const response = await server.post('/api/decks').send({ name: "test deck" });
 
         expect(response.statusCode).toEqual(201)
+        expect(response.body.name).toEqual("test deck")
     })
     it('Should update an existing deck by ID', async () => {
         const response = await server.put('/api/decks/1').send({ name: "updated deck" });
 
         expect(response.statusCode).toEqual(200)
+        expect(response.body.name).toEqual("updated deck")
     })
     it ('Should delete a specific deck by ID', async () => {
         const response = await server.delete('/api/decks/1');
@@ -101,26 +103,31 @@ describe("Card Endpoint Tests", () => {
         const cards = await server.get('/decks/:deckId/cards')
 
         expect(cards.statusCode).toEqual(200)
+        expect(Array.isArray(cards.body)).toEqual(true);
     })
     it('Should respond with specific card on request to /decks/:deckId/cards/:card_id', async () => {
         const cards = await server.get('/decks/:deckId/cards/1')
 
         expect(cards.statusCode).toEqual(200)
+        expect(Array.isArray(cards.body)).toEqual(true);
     })
     it('Should create new card', async () => {
         const response = await server.post('/decks/:deckId/cards').send({ name: "test card" });
 
         expect(response.statusCode).toEqual(201)
+        expect(response.body.name).toEqual("test card")
     })
     it('Should update an existing card by ID', async () => {
         const response = await server.put('/decks/:deckId/cards/1').send({ name: "updated card" });
 
         expect(response.statusCode).toEqual(200)
+        expect(response.body.name).toEqual("updated card")
     })
     it ('Should delete a specific card by ID', async () => {
         const response = await server.delete('/decks/:deckId/cards/1');
 
         expect(response.statusCode).toEqual(200)
+        
     })
 
 })
