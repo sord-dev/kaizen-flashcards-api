53c3cf8f63c5df1a9e9708a17017ef650a31750b
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-04-22 08:46:38 +0100

    adding streak counter and start refactor user stats

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const User = require('../models/user.js')
+const StreakCounter = require('../lib/streakCounter.js')
 
 module.exports.login = async (req, res) => {
     let { body } = req;
@@ -9,11 +10,22 @@ module.exports.login = async (req, res) => {
 
         if (validPw) {
             // check user streak
+            if(StreakCounter.shouldUpdate(user.streak)) {
+                user.streak.incriment()
+                await user.updateStreak()
+            }
+
+            else if(StreakCounter.shouldReset(user.streak)) {
+                user.streak.reset()
+                await user.updateStreak()
+            }
+        
             res.status(200).json({ ...user, password: null })
         } else {
             throw new Error('Incorrect Password')
         }
     } catch (error) {
+        console.log(error);
         return res.status(401).json({ error: error.message })
     }
 }
@@ -26,9 +38,11 @@ module.exports.register = async (req, res) => {
         // hash user password and save
         let hashed = await User.hashPassword(usr.password);
         usr.password = hashed;
-        await usr.save();
 
         // create and assign user streak
+        const userStreak = new StreakCounter(new Date());
+        usr.streak = userStreak;
+        await usr.save();
 
         const user = await User.findByUsername(body.username)
 
@@ -47,22 +61,26 @@ module.exports.index = async (req, res) => {
         res.json({ message: e.message })
     }
 }
-module.exports.addStats = async(req,res)=>{
-    try{
-        const user_id = parseInt (req.body.user_id);
-        const resp = await User.AddMoreToAmount(req.body.amount,req.body.correct,user_id)
-        res.json({Type:"Success"}).status(200)
+
+module.exports.addStats = async (req, res) => {
+    try {
+        const user_id = parseInt(req.body.user_id);
+        const user = await User.findById(user_id);
+
+        const resp = await user.updateStats(req.body.amount, req.body.correct)
+        res.json(resp).status(200)
     }
-    catch(e){
-        res.json({message : e.message}).status(404)
+    catch (e) {
+        res.json({ message: e.message }).status(404)
     }
 }
-module.exports.getUserStats = async(req,res)=>{
-    try{
-        const resp = await User.GetAllStatsByUser(parseInt(req.params.user_id))
+
+module.exports.getUserStats = async (req, res) => {
+    try {
+        const resp = await User.getUserStats(parseInt(req.params.user_id))
         res.json(resp.rows[0]).status(200)
     }
-    catch(e){
-        res.json({message : e.message}).status(404)
+    catch (e) {
+        res.json({ message: e.message }).status(404)
     }
 }
